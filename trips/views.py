0df193a7a783acccf73b8699f57d308c9bf39d3d
leo <leoyenschen@gmail.com>
2017-04-07 16:44:52 +0800

    test

--- trips/views.py
+++ trips/views.py
@@ -14,24 +14,24 @@ from rest_framework import status
 def hello_world(request):
     return render_to_response('hello_world.html',{},context_instance = RequestContext(request))
 
+
 @api_view(['GET', 'POST'])
 def TaskViewSet(request):
     """
     List all snippets, or create a new snippet.
     """
     print("leo test in TaskViewSet")
-    print("request.GET[param1]")
-    print(request.GET['id'])
+    #print("request.GET[param1]")
+    #print(request.GET['id'])
+
     if request.method == 'GET':
         print("leo test in GET")
         Tasks = Task.objects.all()
         serializer = TaskSerializer(Tasks, many=True)
         return Response(serializer.data)
-
+    
     elif request.method == 'POST':
         print("leo test in POST")
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        Tasks = Task.objects.all()
+        serializer = TaskSerializer(Tasks, many=True)
+        return Response(serializer.data)
